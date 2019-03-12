a4c1628f70200cde9f04ca3a480f2bb809b5f91d
StudentMAGe <43290436+StudentMAGe@users.noreply.github.com>
2019-03-12 11:17:25 +0300

    Update tracefoo.py - NOW WORKING!!!

--- tracefoo_mag.py
+++ tracefoo_mag.py
@@ -10,5 +10,5 @@ def tracefoo(userfunc):
 def tracefunc(frame, event, arg):
     if event == "return" and frame.f_code.co_name == nameofuserfunc:
         print("function:", frame.f_code.co_name, ", local vars:", list(frame.f_locals.keys()))
-        return tracefunc
+    return tracefunc
 
